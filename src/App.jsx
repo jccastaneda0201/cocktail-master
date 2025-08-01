cd98a480e999fe7ccdd5fa54dc9eb869f367fca2
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-08-01 17:02:19 +0200

    update functions into cocktail card

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'cocktail/:id',
+        errorElement: <SinglePageError />,
         element: <Cocktail />,
       },
       {
